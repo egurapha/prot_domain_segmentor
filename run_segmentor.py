f7d64909e7d713ea1917d87e2e476ef6cc4b0c29
egurapha <egurapha@gmail.com>
2018-04-23 13:06:12 -0700

    fixed pymol buffering issue, and implemented class in the runfile.

--- run_segmentor.py
+++ run_segmentor.py
@@ -12,59 +12,27 @@ import os, sys
 sys.path.insert(0, 'model')
 from segmentor_model_v2 import *
 from segmentor_utils import *
-
+from DomainSegmentor import *
 
 model_path = 'model/epoch95_model_v2'
 num_classes = 38
-class_dict = {0: 'Unassigned (Loop)', 1: 'Orthogonal Bundle', 2: 'Up-down Bundle', 3: 'Alpha Horseshoe', 4: 'Alpha/alpha barrel', 5: 'Ribbon', 6: 'Aligned Prism', 7: '3-layer Sandwich', 8: '4 Propeller', 9: '5 Propeller', 
-                10: '6 Propeller', 11: '7 Propeller', 12: '2 Solenoid', 13: '3 Solenoid', 14: 'Beta Complex', 15: 'Single Sheet', 16: 'Roll', 17: 'Beta Barrel', 18: 'Clam', 19: 'Sandwich', 20: 'Distorted Sandwich', 21: 'Trefoil', 
-                22: 'Orthogonal Prism', 23: 'Roll', 24: 'Ribosomal Protein L15; Chain: K; domain 2', 25: 'Super Roll', 26: 'Alpha-Beta Barrel', 27: '2-Layer Sandwich', 28: '3-Layer(aba) Sandwich', 29: '3-Layer(bba) Sandwich', 
-                30: '3-Layer(bab) Sandwich', 31: '4-Layer Sandwich', 32: 'Alpha-beta prism', 33: 'Box', 34: '5-stranded Propeller', 35: 'Alpha-Beta Horseshoe', 36: 'Alpha-Beta Complex', 37: 'Irregular', -1: 'NULL'}
-
-def predictPDB(pdb_file, model, ignore_index=-9999):
-    # Initialize Model
-    model.eval()
-    if torch.cuda.is_available(): 
-        model.cuda()
-        print("Running on GPU.")
-    else:
-        print("Running on CPU.")
-    # Generate contact map and get sequence numbering.
-    seq_len, numbering = get_pdb_info(pdb_file)
-    cm, numbering = makeContactMapTensor(pdb_file, seq_len, numbering, target_size=512, upper_tol=512)
-    cm_var = Variable(cm)
-    if torch.cuda.is_available(): 
-        cm_var = cm_var.cuda()
-    outputs = model(cm_var)
-    _, predicted = torch.max(outputs.data, 1)
-    # Format for output.
-    predicted = predicted[0,:,:].cpu().numpy().flatten()
-    out_pred = []
-    out_num = [] 
-    for i in range(len(numbering)):
-        if numbering[i] != ignore_index:
-            out_pred.append(predicted[i])
-            out_num.append(numbering[i])
-    assert len(out_pred) == len(out_num)
-    return out_pred, out_num
-
+class_dict = idx_to_class # stored in DomainSegmentor.py
 
 def segmentFold(input_file, model_path):
-    net = SegmentorModel(1,8,16, num_classes)
-    if torch.cuda.is_available(): 
-        net.load_state_dict(torch.load(model_path))
-    else:
-        net.load_state_dict(torch.load(model_path, map_location=lambda storage, loc: storage))
-    prediction, numbering = predictPDB(input_file, net)
+    segmentor = DomainSegmentor()
+    prediction, numbering = segmentor.predictClass(input_file)
     prediction_dict = {}
     for i in set(prediction):
         prediction_dict[class_dict[i]] = [str(numbering[z]) for z in list(np.where(prediction==i)[0])]
     return prediction_dict
 
-
 if __name__ == "pymol":
+   def splitList(l, n): # Annoying but necessary due to pymol buffering limit.
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
     try:
-        input_file = sys.argv[2] # pdb name # TODO change to commandline argument.
+        input_file = sys.argv[2]
     except:
         raise Exception("No input PDB specified.")
     color_list = ['green', 'violet', 'cyan', 'yellow', 'orange']
@@ -73,16 +41,18 @@ if __name__ == "pymol":
     cmd.color('white', "all")
     print("Running Domain Segmentor.")
     prediction_dict = segmentFold(input_file, model_path=model_path)
-    for i, class_name in enumerate(prediction_dict): # TODO: Color 100 residues at a time otherwise pymol can't buffer it.
+    for i, class_name in enumerate(prediction_dict):
         color = color_list[i%len(color_list)]
-        cmd.color(color, "resi %s" %"+".join(prediction_dict[class_name]))
+        idx_batches = splitList(prediction_dict[class_name], len(prediction_dict[class_name])/100)
+        for idx_list in idx_batches: # Fix for pymol buffering.
+            cmd.color(color, "resi %s" %"+".join(idx_list))
         print(color + " : " + class_name)
 
 if __name__ == '__main__':
     try:
         input_file = sys.argv[1]
     except:
-        raise Exception("No input PDB specified.") # pdb name # TODO change to commandline argument.
+        raise Exception("No input PDB specified.")
     prediction_dict = segmentFold(input_file, model_path=model_path)
     for i, class_name in enumerate(prediction_dict):
         print(class_name)
