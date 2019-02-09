480feccc6412d8c915fa26e0e829f4f404978440
egurapha <egurapha@gmail.com>
2019-02-08 18:29:02 -0800

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# TODO Need to have miniconda or anaconda first.
-conda install -c anaconda biopython  
-conda install -c schrodinger pymol
-conda install numpy scipy
-conda install pytorch torchvision -c pytorch
+conda install -y -c anaconda biopython
+conda install -y -c schrodinger pymol
+conda install -y numpy scipy
+conda install -y pytorch torchvision -c pytorch
+echo "Segmentor Setup is Complete!"
